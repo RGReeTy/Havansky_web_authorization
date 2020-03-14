43de916d33ee63548b9fa46328ceba5bcafd0d47
RGReeTy <be.a.reety@gmail.com>
2020-03-14 22:08:24 +0300

    update bean UserAccount

--- src/com/testAuthorization/bean/UserAccount.java
+++ src/com/testAuthorization/bean/UserAccount.java
@@ -1,41 +1,129 @@
 package com.testAuthorization.bean;
 
-public class UserAccount {
+import java.io.Serializable;
 
-    public static final String GENDER_MALE ="M";
-    public static final String GENDER_FEMALE = "F";
+public class UserAccount implements Serializable {
 
-    private String userName;
-    private String gender;
+    private int id;
+    private String login;
     private String password;
+    private String firstName;
+    private String lastName;
+    private Role role;
 
 
     public UserAccount() {
-
     }
 
-    public String getUserName() {
-        return userName;
+    public UserAccount(int id, String login, String password, String firstName, String lastName, Role role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public final int getId() {
+        return id;
     }
 
-    public String getGender() {
-        return gender;
+    public final void setId(int id) {
+        this.id = id;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
+
+    public final String getFirstName() {
+        return firstName;
     }
 
-    public String getPassword() {
+    public final void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public final String getLastName() {
+        return lastName;
+    }
+
+    public final void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public final String getLogin() {
+        return login;
+    }
+
+    public final void setLogin(String login) {
+        this.login = login;
+    }
+
+    public final String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public final void setPassword(String password) {
         this.password = password;
+    }
+
+    public final Role getRole() {
+        return role;
+    }
+
+    public final void setRole(Role role) {
+        this.role = role;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        UserAccount other= (UserAccount) obj;
+        if (firstName == null) {
+            if (other.firstName != null) return false;
+        } else {
+            if (!firstName.equals(other.firstName)) return false;
+        }
+        if (login == null) {
+            if (other.login != null) return false;
+        } else {
+            if (!login.equals(other.login)) return false;
+        }
+        if (password == null) {
+            if (other.password != null) return false;
+        } else {
+            if (!password.equals(other.password)) return false;
+        }
+        if (role == null) {
+            if (other.role != null) return false;
+        } else {
+            if (role != other.role) return false;
+        }
+
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((login == null) ? 0 : login.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName() + '@' +
+                "id=" + id +
+                ", firstName='" + firstName +
+                ", login='" + login +
+                ", password='" + password +
+                ", role=" + role;
     }
 
 }
