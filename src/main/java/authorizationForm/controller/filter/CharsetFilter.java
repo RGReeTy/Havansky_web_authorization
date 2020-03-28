be909aaa2916509d5f1f9bb1e7f9bfbd26da9d02
RGReeTy <be.a.reety@gmail.com>
2020-03-28 17:33:57 +0300

    added some code

--- src/main/java/authorizationForm/controller/filter/CharsetFilter.java
+++ src/main/java/authorizationForm/controller/filter/CharsetFilter.java
@@ -9,26 +9,27 @@ import java.io.IOException;
         initParams = {@WebInitParam(name = "encoding", value = "UTF-8", description = "Encoding Param")})
 public class CharsetFilter implements Filter {
 
-    private String defaultEncoding = "utf-8";
-    private static final String ENCODING = "encoding";
+    private String code;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        String encoding = filterConfig.getInitParameter(ENCODING);
-        if (encoding != null) {
-            defaultEncoding = encoding;
-        }
+        code = filterConfig.getInitParameter("encoding");
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setCharacterEncoding(defaultEncoding);
-        servletResponse.setCharacterEncoding(defaultEncoding);
-        filterChain.doFilter(servletRequest, servletResponse);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String codeRequest = request.getCharacterEncoding();
+        //setting encoding from filter parameters, if not installed
+        if (code != null && !code.equalsIgnoreCase(codeRequest)) {
+            request.setCharacterEncoding(code);
+            response.setCharacterEncoding(code);
+        }
+        chain.doFilter(request, response);
     }
 
     @Override
     public void destroy() {
-
+        code = null;
     }
 }
